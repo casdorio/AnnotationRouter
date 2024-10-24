248643b67143a97d3c0e737e2eb4b4c7ed75931e
casdorio <casdorio@gmail.com>
2024-10-24 16:06:45 -0300

    s

---
+++ src/Config/Events.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Casdorio\AnnotationRouter\Config;
+
+use CodeIgniter\Config\BaseConfig;
+
+class Events extends BaseConfig
+{
+    public function __construct()
+    {
+        // Aqui você pode adicionar um listener de evento para registrar as anotações
+        \CodeIgniter\Events\Events::on('post_system', function () {
+            $hook = new Hooks();
+            $hook->registerAnnotations();
+        });
+    }
+}
